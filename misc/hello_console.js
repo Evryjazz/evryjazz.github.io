9c8c1093f413800940306b7f634c25d5b10b110a
Evryjazz <julienzanni@gmail.com>
2015-12-14 16:13:14 +0100

    console.log improvement#2

--- misc/hello_console.js
+++ misc/hello_console.js
@@ -1,1 +1,1 @@
-console.log('\r\n%c Hi geekyNerdy, find more info here > https://github.com/Evryjazz/evryjazz.github.io or contact me at hello@julienzanni.fr', 'color:orange');
+console.log('\r\n%c Hi geekyNerdy, find more info here >>', '\r\n%c https://github.com/Evryjazz/evryjazz.github.io' '\r\n%c or contact me at ', '\r\n%c hello@julienzanni.fr', 'color:#F5B82E', 'color:#97DB4F', 'color:#F5B82E', 'color:#97DB4F');
