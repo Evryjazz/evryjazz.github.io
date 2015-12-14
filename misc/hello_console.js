f19f3024c6942ded8673bd4982fefe7ee6ec66ed
Evryjazz <julienzanni@gmail.com>
2015-12-14 16:02:14 +0100

    console.log color

--- misc/hello_console.js
+++ misc/hello_console.js
@@ -1,1 +1,1 @@
-console.log('Hi geekyNerdy, find more info here > https://github.com/Evryjazz/evryjazz.github.io or contact me at hello@julienzanni.fr');
+console.log('Hi geekyNerdy, find more info here > https://github.com/Evryjazz/evryjazz.github.io or contact me at hello@julienzanni.fr', 'color:green');
